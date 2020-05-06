613d70ca7d6a5dcd2978eca9884b7e304441beea
sacridini <eduardolacerdageo@gmail.com>
2020-05-06 00:44:17 -0300

    update

--- code/post-processing/05-zonal.R
+++ code/post-processing/05-zonal.R
@@ -1,5 +1,13 @@
 library(raster)
-r <- raster("~/doutorado/raster/ma_mask.tif")
-ltr <- raster("~/doutorado/raster/ltgee_gain_greatest_2018_seg6/mosaics/mosaic_clip_magnitude.tif")
-z_dur_mean <- raster::zonal(ltr, r, "mean")
-print(z_dur_mean)
+mask <- raster("~/doutorado/raster/ma_mapbiomas_mask.tif")
+ltr_mag <- raster("~/doutorado/raster/ltgee_gain_greatest_2018_seg6/mosaics/mosaic_clip_magnitude.tif")
+z_mag_gain <- raster::zonal(ltr_mag, mask, "mean")
+message(z_mag_gain)
+
+ltr_dur <- raster("~/doutorado/raster/ltgee_gain_greatest_2018_seg6/mosaics/mosaic_clip_duration.tif")
+z_dur_gain <- raster::zonal(ltr_dur, mask, "mean")
+message(z_dur_gain)
+
+ltr_yod <- raster("~/doutorado/raster/ltgee_gain_greatest_2018_seg6/mosaics/mosaic_clip_yod.tif")
+z_yod_gain <- raster::zonal(ltr_yod, mask, "mean")
+message(z_yod_gain)
