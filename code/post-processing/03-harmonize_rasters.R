dfa83a4814a0548940eef4b3b6ee39c07edfbba9
sacridini <eduardolacerdageo@gmail.com>
2020-06-20 06:15:09 -0300

    updated harmonize code to work with new shapefile mask

--- code/post-processing/03-harmonize_rasters.R
+++ code/post-processing/03-harmonize_rasters.R
@@ -1,25 +1,20 @@
-library(raster)
 library(foreach)
 library(doParallel)
 
 files_path <- "~/doutorado/raster/gain_mag_gt200_dur_gt4_preval_gt400_2018/mosaics/"
 setwd(files_path)
 files_to_process = list.files(files_path)
-base_vector_path <- "~/doutorado/vector/mata_atlantica/ma_mapbiomas_shape_fx_diss.shp"
-mask_raster <- raster("~/doutorado/raster/mapbiomas41/bin_masks/bin_crop_mask.tif")
+base_vector_path <- "~/doutorado/vector/mata_atlantica/ma_mask_fixed.shp"
 
 cl = makeCluster(3, outfile = "")
 registerDoParallel(cl)
 
 foreach (f = files_to_process[1:length(files_to_process)]) %dopar% {
   f_out_name <- paste0(gsub(".tif$", "", fs::path_file(f)), "_clip.tif")
-  f <- raster::raster(paste0(files_path, f))
-  raster_masked <- raster::mask(raster::crop(f, mask_raster), mask_raster)
-  raster::writeRaster(paste0("~/doutorado/raster/gain_mag_gt200_dur_gt4_preval_gt400_2018/mosaics/", f_out_name), option = "COMPRESS=DEFLATE")
-  # system(paste0("gdalwarp -cutline ", base_vector_path, 
-  #               " -tr 0.0002694981 0.0002694975 -crop_to_cutline ", 
-  #               f, " ", f_out_name,
-  #               " -co COMPRESS=DEFLATE"))
+  system(paste0("gdalwarp -cutline ", base_vector_path,
+                " -tr 0.0002694981 0.0002694975 -crop_to_cutline ",
+                f, " ", f_out_name,
+                " -co COMPRESS=DEFLATE"))
 }
 
 stopCluster(cl)
