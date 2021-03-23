49e2953e1f89f009e986601bfd9889d3aaa9b931
sacridini <eduardolacerdageo@gmail.com>
2021-03-23 10:41:00 -0300

    update

--- code/results/12_extract_yod_per_year.R
+++ code/results/12_extract_yod_per_year.R
@@ -5,6 +5,7 @@ base_ras <- terra::rast("~/doutorado/testes/loss/finais/yod/loss_masked85_masked
 for (i in 1985:2018) {
   r_tmp <- base_ras
   r_tmp[r_tmp != i] <- NA
+  message(paste0("Saving yod extract for the year ", i))
   terra::writeRaster(r_tmp, paste0("~/doutorado/testes/loss/finais/yod/per_year/", i, ".tif"),
                      wopt=list(gdal=c("COMPRESS=DEFLATE")))
 }
