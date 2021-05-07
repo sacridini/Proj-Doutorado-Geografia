763fdb7f4be0b3e1b142b79205dc645abce9161b
sacridini <eduardolacerdageo@gmail.com>
2021-05-07 13:09:23 -0300

    added gain to elevation analysis

--- code/results/elevation_analysis.R
+++ code/results/elevation_analysis.R
@@ -10,8 +10,8 @@ loss_masked85_maskedGain_bin <- terra::rast("~/doutorado/testes/loss/finais/bin/
 loss_masked85_maskedGain_eq1_bin <- terra::rast("~/doutorado/testes/loss/finais/bin/loss_masked85_maskedGain_dur_eq1_bin.tif")
 loss_masked85_maskedGain_neq1_bin <- terra::rast("~/doutorado/testes/loss/finais/bin/loss_masked85_maskedGain_dur_neq1_bin.tif")
 
-loss_masked85_maskedGain_bin_dem <- dem * loss_masked85_maskedGain_eq1_bin
-terra::writeRaster(loss_masked85_maskedGain_bin, "~/doutorado/testes/elevation_analysis/loss_masked85_maskedGain_bin_dem.tif",
+loss_masked85_maskedGain_bin_dem <- dem * loss_masked85_maskedGain_bin
+terra::writeRaster(loss_masked85_maskedGain_bin_dem, "~/doutorado/testes/elevation_analysis/loss_masked85_maskedGain_bin_dem.tif",
                    wopt=list(gdal=c("COMPRESS=DEFLATE")))
 
 loss_masked85_maskedGain_eq1_bin_dem <- dem * loss_masked85_maskedGain_eq1_bin
@@ -20,4 +20,17 @@ terra::writeRaster(loss_masked85_maskedGain_eq1_bin_dem, "~/doutorado/testes/ele
 
 loss_masked85_maskedGain_neq1_bin_dem <- dem * loss_masked85_maskedGain_neq1_bin
 terra::writeRaster(loss_masked85_maskedGain_neq1_bin_dem, "~/doutorado/testes/elevation_analysis/loss_masked85_maskedGain_neq1_bin_dem.tif",
+                   wopt=list(gdal=c("COMPRESS=DEFLATE")))
+
+# gain
+
+gain_seg6_masked18_dur_gt4_bin <- terra::rast("~/doutorado/testes/gain/finais/bin/gain_seg6_masked18_dur_gt4_bin.tif")
+gain_seg6_masked18_dur_gt4_inv_for_bin <- terra::rast("~/doutorado/testes/gain/finais/bin/gain_seg6_masked18_dur_gt4_inv_for_bin.tif")
+
+gain_seg6_masked18_dur_gt4_bin_dem <- dem * gain_seg6_masked18_dur_gt4_bin
+terra::writeRaster(gain_seg6_masked18_dur_gt4_bin_dem, "~/doutorado/testes/elevation_analysis/gain_seg6_masked18_dur_gt4_bin_dem.tif",
+                   wopt=list(gdal=c("COMPRESS=DEFLATE")))
+
+gain_seg6_masked18_dur_gt4_inv_for_bin_dem <- dem * gain_seg6_masked18_dur_gt4_inv_for_bin
+terra::writeRaster(gain_seg6_masked18_dur_gt4_inv_for_bin_dem, "~/doutorado/testes/elevation_analysis/gain_seg6_masked18_dur_gt4_inv_for_bin_dem.tif",
                    wopt=list(gdal=c("COMPRESS=DEFLATE")))
