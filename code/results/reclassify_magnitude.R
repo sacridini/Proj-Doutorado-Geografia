8880ecd2d4a41952fc296c5be19b21c01f5a79db
sacridini <eduardolacerdageo@gmail.com>
2021-05-16 13:41:52 -0300

    updated reclassify_magnitude.R

--- code/results/reclassify_magnitude.R
+++ code/results/reclassify_magnitude.R
@@ -1,8 +1,9 @@
 library(terra)
 
-file_name <- "gain_seg6_masked18_dur_gt4_albers.tif"
+type <- "loss"
+file_name <- "loss_masked85_maskedGain_albers.tif"
 
-magnitude <- terra::rast(paste0("~/doutorado/testes/gain/finais/albers/", file_name))
+magnitude <- terra::rast(paste0("~/doutorado/testes/", type, "/finais/albers/", tools::file_path_sans_ext(file_name), ".tif"))
 
 # reclassify
 m <- c(0, 200, 1,
@@ -16,7 +17,7 @@ reclass <- terra::classify(magnitude, reclass_table, include.lowest = TRUE)
 
 # save raster
 terra::writeRaster(reclass,
-                   paste0("~/doutorado/testes/gain/finais/albers/analysis_by_class/", tools::file_path_sans_ext(file_name), "_reclass.tif"),
+                   paste0("~/doutorado/testes/", type, "/finais/albers/analysis_by_class/", tools::file_path_sans_ext(file_name), "_reclass.tif"),
                    wopt=list(gdal=c("COMPRESS=DEFLATE")))
 
 # save table
@@ -24,5 +25,5 @@ f <- freq(reclass)
 f_df <- as.data.frame(f)
 f_df$class <- c("0-200", "200-300", "300-400", "400-600", "600-800", "800-")
 write.csv(f_df,
-          paste0("~/doutorado/testes/gain/finais/albers/analysis_by_class/", tools::file_path_sans_ext(file_name), ".csv"),
+          paste0("~/doutorado/testes/", type, "/finais/albers/analysis_by_class/", tools::file_path_sans_ext(file_name), ".csv"),
           sep = ",", row.names = FALSE)
