43423f41be33fd4dfc3b9ab144622e4aa3dea7ca
sacridini <eduardolacerdageo@gmail.com>
2021-06-10 01:42:01 -0300

    updated zonal_summary.R script

--- code/results/zonal_summary.R
+++ code/results/zonal_summary.R
@@ -1,6 +1,5 @@
 library(terra)
 
-
 # GAIN --------------------------------------------------------------------
 # gain_seg6_masked18_dur_gt4_inv_for_albers -------------------------------
 gain_seg6_masked18_dur_gt4_inv_for_albers <-
@@ -21,7 +20,46 @@ summary(gain_seg6_masked18_dur_gt4_inv_for_bin_rate_albers)
 
 
 # LOSS --------------------------------------------------------------------
+# loss_masked85_maskedGain_albers -----------------------------------------
 loss_masked85_maskedGain_albers <-
   terra::vect("~/doutorado/testes/loss/finais/albers/zonal_stats/loss_masked85_maskedGain_albers.gpkg")
 summary(loss_masked85_maskedGain_albers)
+
+
+# loss_masked85_maskedGain_dur_eq1_bin_rate_albers ------------------------
+loss_masked85_maskedGain_dur_eq1_bin_rate_albers <-
+  terra::vect("~/doutorado/testes/loss/finais/albers/zonal_stats/loss_masked85_maskedGain_dur_eq1_bin_rate_albers.gpkg")
+summary(loss_masked85_maskedGain_dur_eq1_bin_rate_albers)
+
+
+# loss_masked85_maskedGain_dur_eq1_mag_albers -----------------------------
+loss_masked85_maskedGain_dur_eq1_mag_albers <-
+  terra::vect("~/doutorado/testes/loss/finais/albers/zonal_stats/loss_masked85_maskedGain_dur_eq1_mag_albers.gpkg")
+summary(loss_masked85_maskedGain_dur_eq1_mag_albers)
+
+
+# loss_masked85_maskedGain_dur_eq1_yod_albers -----------------------------
+loss_masked85_maskedGain_dur_eq1_yod_albers <-
+  terra::vect("~/doutorado/testes/loss/finais/albers/zonal_stats/loss_masked85_maskedGain_dur_eq1_yod_albers.gpkg")
+summary(loss_masked85_maskedGain_dur_eq1_yod_albers)
+
+
+# loss_masked85_maskedGain_dur_neq1_bin_rate_albers -----------------------
+loss_masked85_maskedGain_dur_neq1_bin_rate_albers <-
+  terra::vect("~/doutorado/testes/loss/finais/albers/zonal_stats/loss_masked85_maskedGain_dur_neq1_bin_rate_albers.gpkg")
+summary(loss_masked85_maskedGain_dur_neq1_bin_rate_albers)
+
+
+# loss_masked85_maskedGain_dur_neq1_mag_albers ----------------------------
+loss_masked85_maskedGain_dur_neq1_mag_albers <-
+  terra::vect("~/doutorado/testes/loss/finais/albers/zonal_stats/loss_masked85_maskedGain_dur_neq1_mag_albers.gpkg")
+summary(loss_masked85_maskedGain_dur_neq1_mag_albers)
+
+
+# loss_masked85_maskedGain_dur_neq1_yod_albers ----------------------------
+loss_masked85_maskedGain_dur_neq1_yod_albers <-
+  terra::vect("~/doutorado/testes/loss/finais/albers/zonal_stats/loss_masked85_maskedGain_dur_neq1_yod_albers.gpkg")
+summary(loss_masked85_maskedGain_dur_neq1_yod_albers)
+
+
 
