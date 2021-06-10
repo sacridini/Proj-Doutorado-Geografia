af6438091b9a71b0e0e1c62b038f818888affb34
sacridini <eduardolacerdageo@gmail.com>
2021-06-10 01:35:00 -0300

    updated zonal_summary.R script

--- code/results/zonal_summary.R
+++ code/results/zonal_summary.R
@@ -5,12 +5,23 @@ library(terra)
 # gain_seg6_masked18_dur_gt4_inv_for_albers -------------------------------
 gain_seg6_masked18_dur_gt4_inv_for_albers <-
   terra::vect("~/doutorado/testes/gain/finais/albers/zonal_stats/gain_seg6_masked18_dur_gt4_inv_for_albers.gpkg")
-summary(gain_seg6_masked18_dur_gt4_inv_for_albers$mean)
-summary(gain_seg6_masked18_dur_gt4_inv_for_albers$median)
+summary(gain_seg6_masked18_dur_gt4_inv_for_albers)
 
 # gain_seg6_masked18_dur_gt4_inv_for_yod_albers ---------------------------
 gain_seg6_masked18_dur_gt4_inv_for_yod_albers <-
   terra::vect("~/doutorado/testes/gain/finais/albers/zonal_stats/gain_seg6_masked18_dur_gt4_inv_for_yod_albers.gpkg")
-summary(gain_seg6_masked18_dur_gt4_inv_for_yod_albers$mean)
-summary(gain_seg6_masked18_dur_gt4_inv_for_yod_albers$median)
+summary(gain_seg6_masked18_dur_gt4_inv_for_yod_albers)
+
+
+# gain_seg6_masked18_dur_gt4_inv_for_bin_rate_albers ----------------------
+gain_seg6_masked18_dur_gt4_inv_for_bin_rate_albers <-
+  terra::vect("~/doutorado/testes/gain/finais/albers/zonal_stats/gain_seg6_masked18_dur_gt4_inv_for_bin_rate_albers.gpkg")
+summary(gain_seg6_masked18_dur_gt4_inv_for_bin_rate_albers)
+
+
+
+# LOSS --------------------------------------------------------------------
+loss_masked85_maskedGain_albers <-
+  terra::vect("~/doutorado/testes/loss/finais/albers/zonal_stats/loss_masked85_maskedGain_albers.gpkg")
+summary(loss_masked85_maskedGain_albers)
 
