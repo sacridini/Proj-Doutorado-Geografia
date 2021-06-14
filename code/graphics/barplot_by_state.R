453d27bf0ee74b7bb3c0153f45721086a254c9d1
sacridini <eduardolacerdageo@gmail.com>
2021-06-13 21:35:20 -0300

    updated barplot_by_state script

--- code/graphics/barplot_by_state.R
+++ code/graphics/barplot_by_state.R
@@ -11,11 +11,13 @@ for (i in 1:length(file_list)) {
   v_t <- table(v_base_state@ptr@.xData$df$values())
   vt_dt <- as.data.table(v_t)
   names(vt_dt) <- c("ano", "count")
-  anos <- c("1986","1987","1988","1989","1990", "1991", "1992", "1993", "1994", "1995", "1996", "1997", "1998", "1999",
-            "2000","2001","2002","2003","2004","2005","2006","2007","2008","2009","2010",
-            "2011","2012","2013","2014","2015","2016","2017","2018")
+  anos <- c("1986", "1987", "1988", "1989", "1990", "1991", "1992", "1993", "1994", "1995", "1996",
+            "1997", "1998", "1999", "2000", "2001", "2002", "2003", "2004", "2005", "2006", "2007",
+            "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018")
   barplot(vt_dt$count, names.arg = anos,
           xlab = "Year", ylab = "Number of Pixels",
           main = paste0("Year of detection of all losses with duration equal to 1 - ", v_state_name),
           border = "blue")
+  png(paste0("~/Desktop/ma_estados_vector/", v_state_name, "_loss_eq1.png"), width = 862, height = 364)
+  dev.off()
 }
